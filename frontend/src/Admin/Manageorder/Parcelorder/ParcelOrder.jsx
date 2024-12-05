defa45e54be7e6f08360c9d994ee4fbeb3423e7c
Ayushi246892 <ayushi.paladiya@swiftrut.com>
2024-12-06 01:18:32 +0530

    commit

--- frontend/src/Admin/Manageorder/Parcelorder/ParcelOrder.jsx
+++ frontend/src/Admin/Manageorder/Parcelorder/ParcelOrder.jsx
@@ -116,6 +116,9 @@ const ParcelOrder = () => {
                         <a href='/onsiteorder' className="flex items-center p-2 rounded-md text-gray-300 hover:bg-gray-700">
                             Onsite Order
                         </a>
+                        <a href='/kitchen' className="flex items-center p-2 rounded-md text-gray-300 hover:bg-gray-700">
+                                 Kitchen
+                                </a>
                     </div>
                 )}
             </div>
@@ -225,6 +228,9 @@ const ParcelOrder = () => {
                       <a href='/onsiteorder' className="flex items-center p-2 rounded-md text-gray-300 hover:bg-gray-700">
                           Onsite Order
                       </a>
+                      <a href='/kitchen' className="flex items-center p-2 rounded-md text-gray-300 hover:bg-gray-700">
+                                 Kitchen
+                                </a>
                   </div>
               )}
           </div>
