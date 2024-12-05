909b9783fbe4a9d2191ed21bbbf6e1bb7a998261
Ayushi246892 <ayushi.paladiya@swiftrut.com>
2024-12-06 01:06:13 +0530

    commit

--- frontend/src/Admin/Qrcodes/Createqrcode.jsx
+++ frontend/src/Admin/Qrcodes/Createqrcode.jsx
@@ -319,6 +319,9 @@ const Createqrcode = () => {
                       <a href='/onsiteorder' className="flex items-center p-2 rounded-md text-gray-300 hover:bg-gray-700">
                           Onsite Order
                       </a>
+                      <a href='/kitchen' className="flex items-center p-2 rounded-md text-gray-300 hover:bg-gray-700">
+                        Kitchen
+                       </a>
                   </div>
               )}
           </div>
--- frontend/src/Admin/Qrcodes/QrCode.jsx
+++ frontend/src/Admin/Qrcodes/QrCode.jsx
@@ -189,6 +189,9 @@ function QrCode() {
                 <a href='/onsiteorder' className="flex items-center p-2 rounded-md text-gray-300 hover:bg-gray-700">
                   Onsite Order
                 </a>
+                <a href='/kitchen' className="flex items-center p-2 rounded-md text-gray-300 hover:bg-gray-700">
+                  Kitchen
+                </a>
               </div>
             )}
           </div>
@@ -297,6 +300,9 @@ function QrCode() {
                                 </a>
                                 <a href='/onsiteorder' className="flex items-center p-2 rounded-md text-gray-300 hover:bg-gray-700">
                                   Onsite Order
+                                </a>
+                                <a href='/kitchen' className="flex items-center p-2 rounded-md text-gray-300 hover:bg-gray-700">
+                                 Kitchen
                                 </a>
                               </div>
                             )}
