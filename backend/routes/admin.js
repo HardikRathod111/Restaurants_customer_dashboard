933557eda42532b5082e4215febed417a38bbaa5
Ayushi246892 <ayushi.paladiya@swiftrut.com>
2024-12-10 21:37:02 +0530

    commit

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -1,22 +1,5 @@
 const express = require('express');
 const { admincontroller, adminlogin } = require('../controllers/admin');
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 const router = express.Router();
 
 router.post('/admin',admincontroller);
