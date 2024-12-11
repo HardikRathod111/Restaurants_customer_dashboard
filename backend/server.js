71ebafc5c2013519c5434fa9c8fdec531e0cf9a7
HardikRathod111 <hardik.rathod@swiftrut.com>
2024-12-11 21:12:18 +0530

    server.js

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ connectDB();
 
 app.use(cors({
     origin: [
-        'http://localhost:3000' // Live frontend URL
+        'https://restaurants-customer-dashboard.onrender.com' // Live frontend URL
     ],
     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Adjust methods as needed
     credentials: true // If cookies or authentication tokens are used
